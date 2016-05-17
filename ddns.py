1964bf90d93fb8dee4553af9fb15c5584771b568
KillerInstinct <KillerInstinct@users.noreply.github.com>
2016-05-17 00:11:35 -0400

    Few formatting touch-ups

--- ddns.py
+++ ddns.py
@@ -25,8 +25,9 @@ def getlastip():
     try:
         with open(hist, "r") as old:
             lastip = old.read()
-        logging.debug("Last IP Read from history file: " + lastip)
+        logging.debug("Last IP Read from history file: {0}".format(lastip))
         return lastip
+
     except:
         lastip = "0.0.0.0"
         with open(hist, "w") as old:
@@ -53,14 +54,14 @@ def checkipupdate(ip):
                     new.write(newip)
         
             headers = {"User-Agent": "Google DDNS Updater"}
-            ddns = requests.get("https://domains.google.com/nic/update?hostname="
-                                + host + "&myip=" + newip,
-                                headers=headers, auth=(uid,pwd))
+            url = ("https://domains.google.com/nic/update?hostname={0}"
+                   "&myip={1}".format(host, newip)) 
+            ddns = requests.get(url, headers=headers, auth=(uid,pwd))
             if ddns.status_code == 200:
                 data = ddns.text
                 cmd = data.split()
                 if cmd[0] == "good":
-                    logging.info("IP Updated: " + str(cmd[1]))
+                    logging.info("IP Updated: {0}".format(cmd[0]))
                 if cmd[0] == "nochg":
                     logging.warn("Google reported the same IP as previously "
                                  "reported. Are we incorrectly resending?")
@@ -74,7 +75,7 @@ def checkipupdate(ip):
                 if cmd[0] == "badagent":
                     logging.error("The DDNS client is making bad requests. Ensure "
                                   "the user agent is set in the request and that "
-                                  " you're only attempt to set and IPv4 address.")
+                                  "you're only attempting to set an IPv4 address.")
                 if cmd[0] == "abuse":
                     logging.error("DDNS access for the hostname has been blocked "
                                   "due to failure to interpret previous responses "
@@ -84,8 +85,8 @@ def checkipupdate(ip):
                                   "servers.")
                 return cmd[0]
             else:
-                logging.error("domains.google.com returned status code: " +
-                              str(ddns.status_code))
+                logging.error("domains.google.com returned status code: {0}".format(
+                              ddns.status_code))
                 return "retry"
             
         else:
@@ -98,7 +99,7 @@ logging.info("Starting Google DDNS Client")
 while True:
     check = getlastip()
     run = checkipupdate(check)
-    if run == "wait" or run == "retry" or run == "good":
+    if run in ["wait", "retry", "good"]:
         time.sleep(60)
     else:
         exit()
